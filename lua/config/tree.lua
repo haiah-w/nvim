c3e9cb148155bb58c595118ca6dad4c396daa7d8
haiah-w <103855390+haiah-w@users.noreply.github.com>
2022-11-07 19:04:25 +0800

    Update tree.lua

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -1,5 +1,15 @@
 -- examples for your init.lua
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+    return
+end
 
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+    return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -33,6 +43,11 @@ require("nvim-tree").setup({
                 { key = "f", action = "live_filter" },
                 { key = "F", action = "clear_live_filter" },
                 { key = "p", action = "preview" },
+                { key = { "<cr>", "l", "<2-leftmouse>" }, cb = tree_cb("edit") },
+                { key = "v", cb = tree_cb("vsplit") },
+                { key = "s", cb = tree_cb("split") },
+                { key = "h", cb = tree_cb "close_node" },
+                { key = "o", cb = tree_cb("system_open") },
             },
         },
     },
